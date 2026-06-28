beedd017d8b8331aa087a120974900ecc5435524
MariamKhaled <mariamkhaledqq@gmail.com>
2026-06-28 10:29:12 +0300

    feat: enhance doctor profile layouts and styles

--- code/nfs-frontend/src/App.jsx
+++ code/nfs-frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
+
+import React from 'react';
+import DoctorProfile from './pages/DoctorProfile/DoctorProfile';
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Header />
-
-      <main style={{ flex: 1, padding: '20px' }}>
-        <p>NFS</p>
-      </main>
-
-      <Footer />
+    <div>
+      { }
+      <DoctorProfile />
     </div>
   );
 }
